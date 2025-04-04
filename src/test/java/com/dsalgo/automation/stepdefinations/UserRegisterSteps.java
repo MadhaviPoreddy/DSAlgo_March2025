9bb309cbafc7c8564a2227c31c8c6fd9aa7ed402
kalpanabirkodi <kalpana.birkodi@gmail.com>
2025-04-03 20:11:00 -0500

    Update UserRegisterSteps.java

--- src/test/java/com/dsalgo/automation/stepdefinations/UserRegisterSteps.java
+++ src/test/java/com/dsalgo/automation/stepdefinations/UserRegisterSteps.java
@@ -1,17 +1,6 @@
 package com.dsalgo.automation.stepdefinations;
 
-import java.util.List;
-import java.util.Map;
 
-import org.openqa.selenium.WebDriver;
-
-import com.dsalgo.automation.driver.DriverFactory;
-import com.dsalgo.automation.pages.RegistrationBackgroundPage;
-import com.dsalgo.automation.pages.UserRegisterPage;
-import com.dsalgo.automation.utils.ExcelReader;
-
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
 
 public class UserRegisterSteps {
 
