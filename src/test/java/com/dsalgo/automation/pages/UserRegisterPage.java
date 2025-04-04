6168a34f2773ea2a0dfe83f19c932ac0179027e2
kalpanabirkodi <kalpana.birkodi@gmail.com>
2025-04-03 20:09:54 -0500

    Update UserRegisterPage.java

--- src/test/java/com/dsalgo/automation/pages/UserRegisterPage.java
+++ src/test/java/com/dsalgo/automation/pages/UserRegisterPage.java
@@ -9,42 +9,5 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class UserRegisterPage {
-	private WebDriver driver;
-	
-	@FindBy(id = "id_username")
-	private WebElement userId;
 
-	@FindBy(id = "id_password1")
-	private WebElement userPassword;
-	
-	@FindBy(id = "id_password2")
-	private WebElement confirmPassword;
-
-	@FindBy(xpath = "/html/body/div[2]/div/div[2]/form/input[5]")
-	private WebElement registerButton;
-	
-	@FindBy (xpath = "//html/body/div[3]")
-	private WebElement errorMessage;
-	
-	public UserRegisterPage(WebDriver webDriver) {
-		this.driver = webDriver;
-		PageFactory.initElements(driver, this);
-	}
-
-	public void clickRegister() {
-		registerButton.click();
-	}
-
-	public void verifyUserErrorMsg(String expectedErrorMsg) {
-		String actualMessage =  getMessage(userId);
-		System.out.println("Error message is : " +actualMessage);
-		assertThat(actualMessage).isEqualTo(expectedErrorMsg);
-
-		
-	}
-
-	private String getMessage(WebElement element) {
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		return (String) js.executeScript("return arguments[0].validationMessage;", element);
-	}
 }
