ab6f7e8e47308ed5a81410496a8257973c30db42
kalpanabirkodi <kalpana.birkodi@gmail.com>
2025-04-03 20:09:38 -0500

    Update RegistrationBackgroundPage.java

--- src/test/java/com/dsalgo/automation/pages/RegistrationBackgroundPage.java
+++ src/test/java/com/dsalgo/automation/pages/RegistrationBackgroundPage.java
@@ -6,21 +6,5 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class RegistrationBackgroundPage {
-	private WebDriver driver;
-	
-	@FindBy(xpath = "/html/body/div[1]/div/div/a/button") 
-	private WebElement usernameField;
-	
-	@FindBy (xpath = "//a[@href=\"/register\"]")
-	private WebElement register;
-	
-    public RegistrationBackgroundPage(WebDriver webDriver) {
-        this.driver = webDriver;
-        PageFactory.initElements(driver, this);
-    }
 
-	public void goToRegister() {
-		register.click();
-		
-	}
 }
