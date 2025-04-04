5bc529456abeefc2db3150de8d56c6497ce642e7
kalpanabirkodi <kalpana.birkodi@gmail.com>
2025-04-03 20:12:10 -0500

    Update Hooks.java

--- src/test/java/com/dsalgo/automation/hooks/Hooks.java
+++ src/test/java/com/dsalgo/automation/hooks/Hooks.java
@@ -1,10 +1,5 @@
 package com.dsalgo.automation.hooks;
 
-import com.dsalgo.automation.driver.DriverFactory;
-
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
-import io.cucumber.java.Scenario;
 
 public class Hooks {
 
