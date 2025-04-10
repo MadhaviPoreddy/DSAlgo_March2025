30540fa5ac9cee6c9f444062f34ed6759c97f2b2
kalpanabirkodi <kalpana.birkodi@gmail.com>
2025-04-10 14:24:47 -0500

    Added Code for CrossBr, Excel, Screenshot

--- src/test/java/com/dsalgo/automation/driver/DriverFactory.java
+++ src/test/java/com/dsalgo/automation/driver/DriverFactory.java
@@ -1,40 +1,59 @@
 package com.dsalgo.automation.driver;
 
 import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
+	private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+	// Added Code below for Cross Browser Testing
 	
-	    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+	public static void initializeDriver(String browser) {
+		quitDriver();
 
-	    public static void initializeDriver() {
-	        quitDriver();
+		switch (browser) {
 
-	        ChromeOptions options = new ChromeOptions();
-	        options.addArguments("--disable-extensions");
-	        options.addArguments("--disable-gpu");
-	        options.addArguments("--no-sandbox");
-	        options.addArguments("--remote-allow-origins=*");
+		case "firefox":
+			driver.set(new FirefoxDriver());
+			driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			driver.get().manage().window().maximize();
+
+			break;
+		
+		case "edge":
+
+			driver.set(new EdgeDriver());
+			driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			driver.get().manage().window().maximize();
+			break;
+			
+		case "chrome":
+		default:
+			ChromeOptions options = new ChromeOptions();
+
+//			options.addArguments("--incognito");
 //			options.addArguments("--headless=new");
 
-	        driver.set(new ChromeDriver(options));
-	        driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-	        driver.get().manage().window().maximize();
-	    }
-
-	    public static WebDriver getDriver() {
-	        return driver.get();
-	    }
-
-	    public static void quitDriver() {
-	        if (driver.get() != null) {
-           driver.get().quit();
-            driver.remove();
-	        }
-	    }
+			driver.set(new ChromeDriver(options));
+			driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			driver.get().manage().window().maximize();
+			break;
+		}
 	}
 
+	public static WebDriver getDriver() {
+		return driver.get();
+	}
 
+	public static void quitDriver() {
+		if (driver.get() != null) {
+			driver.get().quit();
+			driver.remove();
+		}
+	}
+}
