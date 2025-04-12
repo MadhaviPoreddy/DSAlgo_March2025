b10005cc91a3571b2f944dbccb574099ad38b0b5
kalpanabirkodi <kalpana.birkodi@gmail.com>
2025-04-11 19:24:25 -0500

    Added Queue Module, POM and CommonStep

--- src/test/java/com/dsalgo/automation/pages/QueuePage.java
+++ src/test/java/com/dsalgo/automation/pages/QueuePage.java
@@ -1,13 +1,30 @@
 package com.dsalgo.automation.pages;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import com.dsalgo.automation.driver.DriverFactory;
 
 public class QueuePage {
 
 	WebDriver driver;
+	
+	@FindBy (id = "id_username")
+	WebElement userName;
+	
+	@FindBy (id = "id_password")
+	WebElement password;
+	
+	@FindBy (xpath = "/html/body/div[2]/div/div[2]/form/input[4]")
+	WebElement loginBtn;
+
+	@FindBy(xpath = "/html/body/div[3]/div[5]/div/div/a")
+	WebElement queueBtn;
 
 	@FindBy(xpath = "//a[text()='Implementation of Queue in Python']")
 	private WebElement implementationOfQPyLnk;
@@ -21,9 +38,33 @@ public class QueuePage {
 	@FindBy(xpath = "//a[text()='Queue Operations']")
 	private WebElement queueOperationLnk;
 
-	public QueuePage(WebDriver webDriver) {
-		this.driver = webDriver;
+	@FindBy(xpath = "//a[text()='Practice Questions']")
+	private WebElement practiceQuestionLnk;
+
+	@FindBy(xpath = "//a[text()='Try here>>>']")
+	public WebElement tryHereBtn;
+
+	@FindBy(css = ".CodeMirror")
+	private WebElement codeEditor;
+
+	@FindBy(xpath = "//button[@type='button']")
+	private WebElement runBtn;
+
+	@FindBy(xpath = "//*[@id='output']")
+	private WebElement output;
+
+	public QueuePage() {
+		this.driver = DriverFactory.getDriver();
 		PageFactory.initElements(driver, this);
+	}
+
+	public void clickQueues() {
+		queueBtn.click();
+	}
+
+	public String verfyQueuePage() {
+		return driver.getTitle();
+
 	}
 
 	public void clickimpOfQPy() {
@@ -34,11 +75,62 @@ public class QueuePage {
 		implementationUsingColDkLnk.click();
 	}
 
-	public void implementationUsingArray() {
+	public void clickImplementationUsingArray() {
 		implementationUsingArrayLnk.click();
 	}
 
-	public void queueOperation() {
+	public void clickQueueOperation() {
 		queueOperationLnk.click();
 	}
-}
+
+	public void practiceQuestions() {
+		practiceQuestionLnk.click();
+	}
+
+	public void tryCodeEditor() {
+		tryHereBtn.click();
+	}
+
+	public void verifyCodeEditor(String code) {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].CodeMirror.setValue(arguments[1]);", codeEditor, code);
+	}
+
+	public void runCode() {
+		runBtn.click();
+	}
+
+	public String verifyOutput() {
+		return output.getText();
+	}
+
+	public void handleAlert() {
+		Alert alert = driver.switchTo().alert();
+		System.out.println("Alert says: " + alert.getText());
+		alert.accept();
+
+	}
+
+	public Boolean alertPresent() {
+		if (ExpectedConditions.alertIsPresent() != null) {
+			return true;
+		}
+		return false;
+	}
+	
+	public String verifyTitleOfPage() {
+		return driver.getTitle();
+	}
+
+	public void enterDetails() {
+//			loginPage.userName.sendKeys("abc@test.com");
+//			loginPage.password.sendKeys("testtest@123");
+//			loginPage.loginBtn.click();
+			
+			userName.sendKeys("abc@test.com");
+			password.sendKeys("testtest@123");
+			loginBtn.click();
+		}
+		
+	}
+
--- src/test/java/com/dsalgo/automation/stepdefinations/CommonStepDef.java
+++ src/test/java/com/dsalgo/automation/stepdefinations/CommonStepDef.java
@@ -1,108 +1,123 @@
 package com.dsalgo.automation.stepdefinations;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+import java.util.Map;
+
+import com.dsalgo.automation.driver.DriverFactory;
+import com.dsalgo.automation.pages.QueuePage;
+import com.dsalgo.automation.pages.TreePage;
+import com.dsalgo.automation.utils.ExcelReader;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class CommonStepDef {
+	
+	QueuePage queuePage = new QueuePage();
+	TreePage treePage = new TreePage();
+	List<Map<String, String>> testData = ExcelReader.getAllRows("CodeEditor");
+	
+    @Given("The user is successfully logged in DsAlgo page")
+    public void the_user_is_successfully_logged_in_ds_algo_page() {
+    	Map<String, String> specificRow = testData.get(0);
+    	String url = specificRow.get("URL");
+    	DriverFactory.getDriver().get(url);
+    	queuePage.enterDetails();
+    }
+    
+    @When("The user clicks on Get Strated link for Queue")
+    public void the_user_clicks_on_get_strated_link_for_queue() {
+    	queuePage.clickQueues();
+    }
+    
+    @When("The user clicks on Get Strated link for Tree")
+    public void the_user_clicks_on_get_strated_link_for_tree() {
+    	treePage.clickTree();
+    }
+    
+    @Given("the user is on the Home page")
+    public void given_the_user_is_on_the_home_page() {
+    	String pageTitle = queuePage.verifyTitleOfPage();
+    	assertEquals(pageTitle, "NumpyNinja");
+    }
 
     @When("The user clicks on Practice questions link")
     public void when_the_user_clicks_on_practice_questions_link() {
-        
+    	queuePage.practiceQuestions();
         
     }
 
     @Then("The User should be navigated to Practice Questions page")
     public void then_the_user_should_be_navigated_to_practice_questions_page() {
-        
+        String practiceTitle = queuePage.verifyTitleOfPage();
+        assertEquals("Practice Questions",practiceTitle);
         
     }
 
     @When("The user clicks on Try Here button")
     public void when_the_user_clicks_on_try_here_button() {
-        
+    	queuePage.tryCodeEditor();
         
     }
 
     @Then("The User should be navigated to the CodeExecutor Editor Page")
     public void then_the_user_should_be_navigated_to_the_codeexecutor_editor_page() {
-        
-        
-    }
-
-    @Given("The User is on Try Editor Page")
-    public void given_the_user_is_on_try_editor_page() {
-        
-        
+    	Map<String, String> specificRow = testData.get(0);
+    	String expectedTitle = specificRow.get("PageTitle");
+        String codeEditorTitle = queuePage.verifyTitleOfPage();
+        assertEquals(expectedTitle,codeEditorTitle);        
     }
 
     @When("The user clicks the Run Button without entering the code in the Editor")
     public void when_the_user_clicks_the_run_button_without_entering_the_code_in_the_editor() {
-        
+    	queuePage.runCode();
         
     }
 
     @Then("The user is not able to see an error message")
     public void then_the_user_is_not_able_to_see_an_error_message() {
-        
+    	Map<String, String> specificRow = testData.get(0);
+    	String expectedOutput = specificRow.get("Output");
+        String codeOutput = queuePage.verifyOutput();
+        assertEquals(expectedOutput,codeOutput);
         
     }
 
-    @When("The user enters a valid Python code in Editor and click  on the Run button")
+    @When("The user enters a valid Python code in Editor and click on the Run button")
     public void when_the_user_enters_a_valid_python_code_in_editor_and_click_on_the_run_button() {
-        
-        
+    	Map<String, String> specificRow = testData.get(1);
+    	String validCode = specificRow.get("ValidCode");
+    	System.out.println(validCode);
+    	queuePage.verifyCodeEditor(validCode);
+    	queuePage.runCode();
     }
 
     @Then("The user should be able to execute the code")
     public void then_the_user_should_be_able_to_execute_the_code() {
-        
-        
+    	Map<String, String> specificRow = testData.get(1);
+    	String expectedOutput = specificRow.get("Output");
+        String codeOutput = queuePage.verifyOutput();
+        assertEquals(expectedOutput,codeOutput);
     }
 
     @When("The user enters an invalid code in Editor and click on the Run button")
     public void when_the_user_enters_an_invalid_code_in_editor_and_click_on_the_run_button() {
-        
-        
-    }
+    	Map<String, String> specificRow = testData.get(2);
+    	String invalidCode = specificRow.get("InvalidCode");
+    	System.out.println(invalidCode);
+    	queuePage.verifyCodeEditor(invalidCode);
+    	queuePage.runCode();
+    }    
 
     @Then("The user should get an error message after excuting the code")
     public void then_the_user_should_get_an_error_message_after_excuting_the_code() {
+    	Boolean isDisplayed = queuePage.alertPresent();
+    	assertTrue(isDisplayed);
+    	queuePage.handleAlert();
         
-        
     }
-    
-    @Given("the user is on the DS Algo Sign-In Page")
-    public void theUserIsOnSignInPage() {
-       
-    }
-    
-    @When("the user clicks the Login button")
-    public void theUserClicksLoginButton() {
-     
-    }
-    
-    @Then("the user should be redirected to the Data Structure Home Page")
-    public void verifyRedirectionToHomePage() {
-       
-    }
-    
-    @Then("user should be redirected to the Home Page")
-    public void user_should_be_redirected_to_the_home_page() {
-
-    }
-    
-	@Given("the user is on the Home page")
-	public void the_user_is_on_the_home_page() {
-
-	}
-	
-	@When("The user clicks on Register link")
-	public void the_user_clicks_on_register_link() {
-
-	}
-	@Then("the user should be navigated to Registration page")
-	public void the_user_should_be_navigated_to_registration_page() {
-
-	}
 }
