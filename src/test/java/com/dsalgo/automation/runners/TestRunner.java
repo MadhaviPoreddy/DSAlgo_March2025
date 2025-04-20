931dad7cc68ce796cfad939ed02766d73ca11365
MadhaviPoreddy <mporeddy1014@gmail.com>
2025-04-19 22:06:45 -0400

    madhavi files with test data

--- src/test/java/com/dsalgo/automation/runners/TestRunner.java
+++ src/test/java/com/dsalgo/automation/runners/TestRunner.java
@@ -1,10 +1,11 @@
 package com.dsalgo.automation.runners;
 
 import org.junit.runner.RunWith;
-import org.testng.annotations.DataProvider;
+//import org.testng.annotations.DataProvider;
 import io.cucumber.junit.Cucumber;
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
+//import io.cucumber.testng.AbstractTestNGCucumberTests;
+//import io.cucumber.testng.CucumberOptions;
+import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
@@ -12,17 +13,18 @@ import io.cucumber.testng.CucumberOptions;
     glue = {"com.dsalgo.automation.stepdefinations", "com.dsalgo.automation.hooks"}, // Location of the step definition classes
     plugin = {"pretty", "html:target/cucumber-reports.html","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
 			"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
-	tags = "", //tags from feature file
+	//tags = "@ll", //tags from feature file
     monochrome = true// Reports
     )
 
-public class TestRunner extends AbstractTestNGCucumberTests{
+public class TestRunner {
+//public class TestRunner extends AbstractTestNGCucumberTests{
 	
-	@Override
-    @DataProvider(parallel = true)
-    public Object[][] scenarios() {
-				
-		return super.scenarios();
-    }
+//	@Override
+//    @DataProvider(parallel = false)
+//    public Object[][] scenarios() {
+//				
+//		return super.scenarios();
+//    }
 
 }
