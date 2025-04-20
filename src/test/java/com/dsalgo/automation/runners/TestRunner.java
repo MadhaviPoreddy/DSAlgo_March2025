5f08435a9e13f3cb4ff47526b4389758317be4f4
kalpanabirkodi <kalpana.birkodi@gmail.com>
2025-04-20 18:24:10 -0500

    Added testrunner for cucumber

--- src/test/java/com/dsalgo/automation/runners/TestRunner.java
+++ src/test/java/com/dsalgo/automation/runners/TestRunner.java
@@ -1,18 +1,31 @@
 package com.dsalgo.automation.runners;
 
 import org.junit.runner.RunWith;
+//import org.testng.annotations.DataProvider;
 import io.cucumber.junit.Cucumber;
+//import io.cucumber.testng.AbstractTestNGCucumberTests;
+//import io.cucumber.testng.CucumberOptions;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features/Register.feature",
-		glue = { "com.dsalgo.automation.stepdefinations", "com.dsalgo.automation.hooks" }, 																							
-		plugin = { "pretty", "html:target/cucumber-reports.html",
-				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
-				}, 
-		monochrome = true
-)
+@CucumberOptions(
+    features = "src/test/resources/features/Register.feature", // Location of the feature files
+    glue = {"com.dsalgo.automation.stepdefinations", "com.dsalgo.automation.hooks"}, // Location of the step definition classes
+    plugin = {"pretty", "html:target/cucumber/cucumber-reports.html","json:target/cucumber/cucumber.json",
+    		"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+			"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+	tags = "", //tags from feature file
+    monochrome = true// Reports
+    )
+
 public class TestRunner {
+//public class TestRunner extends AbstractTestNGCucumberTests{
+	
+//	@Override
+//    @DataProvider(parallel = false)
+//    public Object[][] scenarios() {
+//				
+//		return super.scenarios();
+//    }
 
 }
